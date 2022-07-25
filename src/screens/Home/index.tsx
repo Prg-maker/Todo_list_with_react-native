7f6287910dfa8a2700900e31a1f56fdc5a456be6
Prg-maker <df3296709@gmail.com>
2022-07-25 10:18:39 -0300

    fist commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import {styled} from './style'
+
+export function Home(){
+  return(
+    <View style={styled.container}>
+        <Text>Hello word</Text>
+    </View>
+  )
+}
+
---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+export default {
+  colors: {
+    color_text: '#000000',
+    color_background: '#fff',
+    color_button: '#9DC2CE',
+    color_link:'#00C1FF',
+    
+  }
+}
